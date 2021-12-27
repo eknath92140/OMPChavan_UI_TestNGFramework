c2f97242022dc9b2a1f8f3251e889e99af250ab1
Parth <parth92140@gmail.com>
2021-12-27 14:24:57 +0530

    updated tC

--- src/test/java/org/selenium/pom/tests/AddToCartTest.java
+++ src/test/java/org/selenium/pom/tests/AddToCartTest.java
@@ -29,7 +29,7 @@ public class AddToCartTest extends BaseTest {
        CartPage cartPage= new StorePage(getDriver()).load()
                .getProductThumbnail().clickAddToCartBtn(product.getName())
                .clickViewCart();
-        Assert.assertEquals(cartPage.getProductName(),product.getName()+"Test");
+        Assert.assertEquals(cartPage.getProductName(),product.getName());
     }
 
     @Test(dataProvider = "getFeaturedProduct", dataProviderClass = MyDataProviders.class)
